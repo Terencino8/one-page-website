335fdb3a5908eeaac4435faf0b1e4a7b2217899f
Terence Onyekachi <87383877+Terencino8@users.noreply.github.com>
2023-04-02 08:31:27 +0100

    done with the features

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import "./Features.css"
+import { BsFillBookmarkStarFill } from 'react-icons/bs'
+import phoneFeatures from "../../assets/phone-features.png"
+import Feature from './Feature.js'
+import { featureList } from "./data"
 
 const Features = () => {
   return (
-    <div>
-      <h1>features</h1>
-    </div>
+    <section id='features'>
+      
+      <div className="container features">
+        <div className="u-title">
+          <BsFillBookmarkStarFill color='orangered' size={30}/>
+          <h2>Core Features</h2>
+          <p className="u-text-small u-text-dark">
+            TERRy.C is a Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum maiores aspernatur non. Laudantium culpa architecto quisquam rem porro molestias
+            </p>
+        </div>
+
+        <div className="features-content">
+            <div className="features-left">
+                <img src={phoneFeatures} alt="Phone" />
+            </div>
+
+            <div className="features-right">
+            {
+                featureList.map((feature) =>(
+                  <Feature key={feature.id} icon={feature.icon} heading={feature.heading} text={feature.text}/>
+                ))
+            }
+            </div>
+        </div>
+
+      </div>
+
+    </section>
   )
 }
 
---
+++ src/components/features/data.js
@@ -0,0 +1,39 @@
+import {FaAccessibleIcon, FaGg} from "react-icons/fa";
+import {SiDatabricks} from "react-icons/si"
+import {MdConnectWithoutContact} from "react-icons/md"
+
+
+
+export const featureList = [
+    {
+        id: 1, 
+        icon: <FaAccessibleIcon color="#0a1930" size={22} />,
+        heading: "Designed for You",
+        text:"    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis molestiae necessitatibus delectus alias iste doloremque sequi ex. Quisquam officia autem atque sint, voluptatibus minima, vero repellendus numquam quasi nam fugiat",
+    
+    },
+
+    {
+        id: 2, 
+        icon: <SiDatabricks color="#0a1930" size={22} />,
+        heading: "Keep Your History",
+        text:"    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis molestiae necessitatibus delectus alias iste doloremque sequi ex. Quisquam officia autem atque sint, voluptatibus minima, vero repellendus numquam quasi nam fugiat",
+    
+    },
+
+    {
+        id: 3, 
+        icon: <MdConnectWithoutContact color="#0a1930" size={22} />,
+        heading: "Stay Connected",
+        text:"    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis molestiae necessitatibus delectus alias iste doloremque sequi ex. Quisquam officia autem atque sint, voluptatibus minima, vero repellendus numquam quasi nam fugiat",
+    
+    },
+
+    {
+        id: 3, 
+        icon: <FaGg color="#0a1930" size={22} />,
+        heading: "Take COntrol",
+        text:"    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis molestiae necessitatibus delectus alias iste doloremque sequi ex. Quisquam officia autem atque sint, voluptatibus minima, vero repellendus numquam quasi nam fugiat",
+    
+    }
+]
