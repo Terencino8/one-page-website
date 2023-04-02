428c9ed3b3e0f74d258db3488d2a9435798ae8d8
Terence Onyekachi <87383877+Terencino8@users.noreply.github.com>
2023-04-02 23:10:39 +0100

    finished the download section

--- src/components/downloads/Download.js
+++ src/components/downloads/Download.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import "./Download.css"
+import {GrAndroid} from "react-icons/gr"
+import {FaApple, FaWindows} from "react-icons/fa"
+import {IconContext} from "react-icons"
+
+
+
 
 const Download = () => {
   return (
-    <div>
-       <h1>download</h1>
-    </div>
+    <section id='download'>
+        <div className="container download">
+            <h2>Terry.C MESSANGER DOWNLOAD</h2>
+            <p className="u-text-small u-text-light">
+                Our apps are available for download on all stores.
+            </p>
+
+
+            
+          <IconContext.Provider value={{size:"25"}}>
+            <div className="download-icons">
+                  <div className="download-icon">
+                    <FaApple/> <p>ios</p>
+                  </div>
+
+                  <div className="download-icon">
+                    <GrAndroid/> <p>Android</p>
+                  </div>
+
+                  <div className="download-icon">
+                    <FaWindows/> <p>Windows</p>
+                    
+                </div>
+              </div>
+          </IconContext.Provider>
+        </div>       
+    </section>
   )
 }
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
           <ul className="nav-links" id={showMenu ? "nav-link-mobile" : "nav-link-mobile-hide"}>
             <li><a href="#">Home</a></li>
             <li><a href="#features">Features</a></li>
-            <li><a href="#">Download</a></li>
+            <li><a href="#download">Download</a></li>
             <li><a href="#">Subscribe</a></li>
             {/* <li className='nav-btn'><a href="#" className='btn btn-dark'>Get Started</a></li> */}
 
