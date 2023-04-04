5c8a592b35b6a7baaa2cc9225538c7527d8b0bde
Terence Onyekachi <87383877+Terencino8@users.noreply.github.com>
2023-04-04 23:02:53 +0100

    started creating the faq page and  had issues with the data jas for the faq we continue tmr

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { MdOutlineLibraryBooks } from 'react-icons/md'
+
+
 
 const Faq = () => {
   return (
-    <div>
-        <h1>faqs</h1>
-    </div>
+    <section id='faq'>
+        <div className="container faq">
+        <div className="u-title">
+          <MdOutlineLibraryBooks color='orangered' size={30}/>
+          <h2>FAq</h2>
+          <p className="u-text-small u-text-dark">
+            TERRy.C is a Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum maiores aspernatur non. Laudantium culpa architecto quisquam rem porro molestias
+            </p>
+        </div>
+        </div>
+    </section>
   )
 }
 
---
+++ src/components/faq/data.js
@@ -0,0 +1,23 @@
+export const questions = {
+    {
+        id: 1,
+        title: "Can I Learn REactJS",
+        answer: "Yes, you can learn ReactJs at My Place" ,
+    },
+    {
+        id: 2,
+        title: "How Can i get started with Terry",
+        answer: "Yes, you can learn ReactJs at My Place" ,
+    },
+    {
+        id: 3,
+        title: "Can I Learn REactJS",
+        answer: "Yes, you can learn ReactJs at My Place" ,
+    },
+    {
+        id: 4,
+        title: "Can I Learn REactJS",
+        answer: "Yes, you can learn ReactJs at My Place" ,
+    },
+    
+}
