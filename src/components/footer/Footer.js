f486d6e0abd594f0765e162aa35ab957c4b2dd93
Terence Onyekachi <87383877+Terencino8@users.noreply.github.com>
2023-04-06 23:54:19 +0100

    just started the footer page today finishing it tmr

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import "./Footer.css"
+import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope, FaGlobe, } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
-        <h1>footer</h1>
+    <div id='footer'>
+      <div className="container footer">
+
+        <div className="footer-box">
+            <h4>Useful Links</h4>
+            <div className="footer-links">
+            <a href="#">&bull; Support</a>
+            <a href="#">&bull; About</a>
+            <a href="#">&bull; Learn</a>
+            <a href="#">&bull; Hosting</a>
+            <a href="#">&bull; Messanger</a>
+            </div>
+
+        </div>
+        <div className="footer-box">
+              <h4>Support</h4>
+              <div className="footer-links">
+                <a href="#">&bull; Support</a>
+                <a href="#">&bull; About</a>
+                <a href="#">&bull; Learn</a>
+                <a href="#">&bull; Hosting</a>
+                <a href="#">&bull; Messanger</a>
+              </div>
+        </div>
+        <div className="footer-box">
+          <h4>Contact Us</h4>
+          .footer-contact
+        </div>
+        <div className="footer-box"></div>
+        
+      </div>
+        
     </div>
   )
 }
